89cf11101c5dfaa7ae7502d83860e5e49c405f24
Fabio Maiolo <fabio_maiolo@hotmail.com>
2020-10-03 20:56:26 -0300

    models, migrations and seed created

---
+++ app/models/contact.rb
@@ -0,0 +1,4 @@
+class Contact < ApplicationRecord
+
+  validates :email, uniqueness: true
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Contact.destroy_all
+
+puts "destroyed all contacts"
+
+puts "creating new contacts"
+5.times do
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  full_name = "#{first_name} #{last_name}"
+  var_email = Faker::Internet.email(name: full_name,separators: '.', domain: "example")
+  contact = Contact.create!(name: full_name, email: var_email) 
+  puts "Name: #{contact.name}"
+  puts "E-Mail: #{contact.email}"
+end
